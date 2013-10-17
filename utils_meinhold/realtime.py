030d19a9bca0b0714c4560ffc2b702971dee34ed
Peter Meinhold <peterm@deepspace.ucsb.edu>
2013-10-17 16:45:30 -0700

    more

--- utils_meinhold/realtime.py
+++ utils_meinhold/realtime.py
@@ -184,7 +184,7 @@ def plotnow(yrmoday,fpath='',chan='ch2'):
     plt.xlabel('Azimuth angle, degrees')
     plt.ylabel('Signal, V')
     plt.title(chan+' COFE data binned to azimuth, file: '+fld[-1])
-    legend()
+    plt.legend()
     plt.grid()
     plt.show()
     return combined
@@ -201,12 +201,13 @@ def plotrawnow(yrmoday,fpath='',chan='ch2'):
     if stats.st_size == 10752000:
         dr=demod.read_raw([fld[-1]])
         for i in range(0,np.shape(dr[chan])[0],50):
-            plt.plot(dr[chan][i,:])
-            plt.xlabel('encoder position')
-            plt.ylabel('Signal, V')
-            plt.title('Ch2 raw data, every 50 revs, file: '+fld[-1])
-            plt.grid()
-            plt.show()
+            plt.plot(dr[chan][i,:],legend='rev '+str(i))
+        plt.xlabel('encoder position')
+        plt.ylabel('Signal, V')
+        plt.title('Ch2 raw data, every 50 revs, file: '+fld[-1])
+        plt.legend()
+        plt.grid()
+        plt.show()
     return dr
     
 def psmapcurrent(cdata,chan='ch2',cmode='T',nbins=360):
